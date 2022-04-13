d952c923b6d69b62b63ef6b3205f18c7416b0210
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2022-04-12 23:35:44 -0300

    Switch to built-in ref management
    Also, taggedPointers were dropped because we will probably want CTFE.

--- source/gyre/graph.d
+++ source/gyre/graph.d
@@ -4,6 +4,8 @@ Graph construction and manipulation procedures.
 When expressed in Gyre, programs become sets of nodes and (directed) edges.
 It is very important that these graphs have a low memory footprint and enable the efficient implementation of program transformations.
 Following the design of [Click's sea of nodes](https://www.oracle.com/technetwork/java/javase/tech/c2-ir95-150110.pdf), our graphs are represented as mutable objects with (possibly cyclic) references to each other.
+The current implementation relies on D's automatic memory management, mainly because managed references (i.e. any variable of a class type) are kept stable by the runtime.
+The alternative would be to use pointers to struct types, which would basically require us to implement a special-purpose moving GC with either double indirections or updates *everywhere* whenever a node is moved (e.g. when growing a dynamic array, when an AA is rehashed, etc).
 
 Gyre follows SSA form (but not necessarily minimal SSA).
 This implies that values must be produced before they are used (def-before-use property) and that values and "variable names" have a one-to-one correspondence (single-assignment property).
@@ -19,12 +21,7 @@ See_Also:
 +/
 module gyre.graph;
 
-import std.bitmanip : taggedPointer;
-import std.conv : to;
-import std.format : format;
-import std.traits : EnumMembers;
-
-import set;
+import gyre.set : Set;
 
 
 /++
@@ -70,15 +67,22 @@ See_Also:
 +/
 enum EdgeKind : ubyte { control, data, memory }
 
-private mixin template StaticEdgeBuilders(string slotType) {
+// XXX: this enum is better kept with at most 2 bits of information: we could
+// pack it inside edge slots through a taggedPointer, but we're currently not
+// doing this because those are incompatible with CTFE (due to pointer casts)
+static assert(EdgeKind.min >= 0 && EdgeKind.max < 4, "EdgeKind should have at most 2 useful bits");
+
+private mixin template StaticEdgeFactories(alias SlotType) {
+    private import std.conv : to;
+    private import std.format : format;
+    private import std.traits : EnumMembers;
     static foreach (kind; EnumMembers!EdgeKind) {
         mixin(q{
-            static %1$s %2$s() @nogc nothrow pure @safe {
-                %1$s e;
-                e.kind = EdgeKind.%2$s;
+            static %1$s %2$s() nothrow pure @safe {
+                auto e = new %1$s(EdgeKind.%2$s);
                 return e;
             }
-        }.format(slotType, to!string(kind)));
+        }.format(SlotType.stringof, to!string(kind)));
     }
 }
 
@@ -86,96 +90,68 @@ private mixin template StaticEdgeBuilders(string slotType) {
 An outgoing edge slot.
 
 An OutEdge connects to zero or one [InEdge].
-It can be used as a pointer to its target edge slot.
 
 See_Also:
 [InEdge]
 +/
-struct OutEdge {
-    mixin(taggedPointer!(
-        InEdge*, "target", /// Either points to another edge slot, or is null.
-        EdgeKind, "kind", 2 /// This edge's kind, usually fixed on construction.
-    ));
+final class OutEdge {
+    InEdge target; /// Either points to another edge slot, or is null.
+    const EdgeKind kind; /// This edge's kind, fixed on construction.
 
-    mixin StaticEdgeBuilders!(this.stringof);
-
-    ref inout(InEdge) opUnary(string op)() inout @nogc nothrow pure if (op == "*")
-    in {
-        assert(this.target != null);
-    } do {
-        return *this.target;
+    this(EdgeKind kind, InEdge target = null) nothrow pure @safe {
+        this.kind = kind;
+        this.target = target;
     }
 
-    void opAssign(return InEdge* rhs) @nogc nothrow pure @safe
-    out {
-        assert(rhs == null || rhs.kind == this.kind);
-    } do {
-        this.target = rhs;
+    mixin StaticEdgeFactories!(typeof(this));
+
+    invariant {
+        assert(target is null || target.kind == this.kind);
     }
 }
 
 ///
-@nogc nothrow pure unittest {
+nothrow pure @safe unittest {
     // data edges are directed from uses to defs
-    auto use = OutEdge.data;
     auto def = InEdge.data;
-    assert(use.target == null); // initially, target is null
-
-    // assigning a target adress will set the target field
-    use = &def;
-    assert(use.target == &def);
-
-    // dereferencing an OutEdge will yield a ref to its target
-    assert(*use == def);
+    auto use1 = OutEdge.data;
+    auto use2 = OutEdge.data;
+    use1.target = def;
+    use2.target = def;
+    assert(use1.target is def);
+    assert(use2.target is def);
 }
 
 /++
 An incoming edge slot.
 
 InEdge slots can receive zero or more [OutEdge]s.
-May be used as a pointer to its owner [Node].
 
 See_Also:
 [Node]
 +/
-struct InEdge {
-    mixin(taggedPointer!(
-        Node*, "owner", /// Points back to the [Node] which owns this edge.
-        EdgeKind, "kind", 2 /// This edge's kind, usually fixed on construction.
-    ));
+final class InEdge {
+    Node owner; /// Points back to the [Node] which owns this edge.
+    const EdgeKind kind; /// This edge's kind, fixed on construction.
 
-    mixin StaticEdgeBuilders!(this.stringof);
-
-    ref inout(Node) opUnary(string op)() inout @nogc nothrow pure if (op == "*")
-    in {
-        assert(this.owner != null);
-    } do {
-        return *this.owner;
+    this(EdgeKind kind, Node owner = null) nothrow pure @safe {
+        this.kind = kind;
+        this.owner = owner;
     }
 
-    void opAssign(return Node* rhs) @nogc nothrow pure @safe {
-        this.owner = rhs;
-    }
+    mixin StaticEdgeFactories!(typeof(this));
 }
 
 ///
-@nogc nothrow pure unittest {
+nothrow pure @safe unittest {
     // in this example, control flows out from A and into B
+    auto node = new Node();
     auto A = OutEdge.control;
     auto B = InEdge.control;
-    A.target = &B;
-    assert(B.owner == null); // initially, node is null
-
-    // dereferencing an OutEdge will yield a ref to its owner
-    Node node;
-    B = &node;
-    assert(B.owner == &node);
-    assert(*B == node);
-    assert(**A == node); // A (out) -> B (in) -> node (contains B)
+    A.target = B;
+    B.owner = node;
+    assert(A.target.owner is node);
 }
-
-// edge slots are no heavier than pointers
-static assert(OutEdge.sizeof == (Node*).sizeof && InEdge.sizeof <= OutEdge.sizeof);
 
 
 /++
@@ -187,7 +163,52 @@ Meanwhile, the set of out-neighbors is implicitly defined by a Node's [OutEdge] 
 See_Also:
 [Hash sets](./set.html)
 +/
-struct Node {
+final class Node {
+    Set!(Node) observers;
     // TODO
-    Set!(Node*) observers;
+    // override bool opEquals(Object rhs) const;
+    // override size_t toHash() const nothrow @trusted;
 }
+
+struct JoinNode {
+    auto definition = InEdge.data;
+    auto control = OutEdge.control;
+    private alias Parameters = InEdge[];
+    Parameters[] pattern;
+}
+
+struct InstantiateNode {
+    auto definition = OutEdge.data;
+    InEdge[] continuations;
+}
+
+struct ContinueNode {
+    auto control = InEdge.control;
+    auto continuation = OutEdge.data;
+    OutEdge[] values;
+}
+
+struct IfNode {
+    auto control = InEdge.control;
+    auto selector = OutEdge.data;
+    auto thenBranch = OutEdge.control;
+    auto elseBranch = OutEdge.control;
+}
+
+struct ConcurrentNode {
+    auto control = InEdge.control;
+    bool explicit = false;
+    auto leftFork = OutEdge.control;
+    auto rightFork = OutEdge.control;
+}
+
+struct MacroNode {
+    OutEdge[] dataUses;
+    InEdge[]  dataDefs;
+    InEdge[]  controlIncoming;
+    OutEdge[] controlOutgoing;
+    auto minThreads = OutEdge.data;
+    auto maxThreads = OutEdge.data;
+}
+
+// TODO: prim ops { literals, arithmetic, mem ops }
---
+++ source/gyre/set.d
@@ -0,0 +1,201 @@
+/++
+Hash-based sets in a standalone (and single-file) module.
+
+License: Zlib
+Authors: [Gabriel B. Sant'Anna](baiocchi.gabriel@gmail.com)
++/
+module gyre.set;
+
+import std.range.primitives : ElementType, isInputRange, isInfinite;
+import std.traits : isPointer;
+
+
+/++
+Generic hash-based set.
+
+The mechanism used to override hashing and comparison functions is [the same as for standard AAs](https://dlang.org/spec/hash-map.html#using_struct_as_key).
+Unlike AAs, however, **storing pointer types will call the pointed-to-type's custom hash function** instead (if it has one).
+
+NOTE: Like AAs, Sets should be treated as a reference type (even if it's a struct).
++/
+struct Set(T) {
+    private {
+        // this is essentially a wrapper around a classic AA trick, but with a twist
+        alias Unit = void[0];
+        enum unit = Unit.init;
+        static if (isPointer!T) alias Key = HashablePointer!(typeof(*T));
+        else                    alias Key = T;
+        Unit[Key] aa;
+    }
+    nothrow pure @safe {
+        /// Adds an element to the set.
+        void add(T x)
+        out {
+            assert(x in this, "added element should be in the set");
+        } do {
+            Key key = x;
+            this.aa[key] = unit;
+        }
+
+        /++
+        Removes an element from the set.
+        Returns: Whether or not the element was present in the set.
+        +/
+        bool remove()(auto ref T x)
+        out {
+            assert(x !in this, "removed element should not be in the set");
+        } do {
+            Key key = x;
+            return this.aa.remove(key);
+        }
+
+        /// Checks if an element is present in the set. Equivalent to the expression `x in set`
+        bool contains()(auto ref T x) const {
+            Key key = x;
+            return (key in this.aa) != null;
+        }
+
+        bool opBinaryRight(string op)(auto ref T lhs) const if (op == "in") {
+            return this.contains(lhs);
+        }
+
+        /// Returns the number of elements in the set.
+        @property size_t length() const @nogc {
+            return this.aa.length;
+        }
+
+        /// Returns a forward range which will iterate over the set's elements.
+        auto byKey() const @nogc {
+            return this.aa.byKey;
+        }
+    }
+
+    /// Rehashes the set in place (so that lookups are more efficient).
+    auto rehash() nothrow pure {
+        return this.aa.rehash();
+    }
+
+    /// Returns an independent duplicate of this set.
+    Set!T dup()() const // templated for attribute inference
+    out (result) {
+        assert(result.length == this.length, "duplicate set should have the same size as the original");
+        foreach (ref T x; this) assert(x in result, "every element in the original should be in the new set");
+    } do {
+        import std.algorithm : map;
+        import std.traits : hasMember;
+        return this.byKey.map!((T x) {
+            static if (hasMember!(T, "dup")) return x.dup;
+            else                             return x;
+        }).set;
+    }
+
+    int opApply(F)(F iter) const { // templated for attribute inference
+        foreach (ref T x; this.byKey) {
+            int stop = iter(x);
+            if (stop) return 1;
+        }
+        return 0;
+    }
+}
+
+///
+nothrow pure unittest {
+    import std.algorithm;
+
+    bool usingCustomHash = false;
+    struct MyInt {
+        int value;
+     @nogc nothrow pure @safe:
+        size_t toHash() const {
+            debug usingCustomHash = true;
+            return value * value;
+        }
+        bool opEquals()(auto ref const(MyInt) other) const {
+            return this.value == other.value;
+        }
+    }
+
+    static immutable list = [0, 1, 42, int.max, int.min, 0];
+    enum N = list.length;
+    Set!(MyInt*) s = list.map!(n => new MyInt(n)).set;
+    assert(s.length == N-1); // one less because of the duplicate (0)
+    s.add(null);
+    assert(s.length == N);
+
+    // structural equality works
+    auto duplicate = s.dup;
+    s.rehash;
+    assert(s == duplicate);
+
+    auto forty2 = new MyInt(42); // notice the different allocation
+    assert(forty2 in s);
+    assert(s.remove(forty2) == true); // actually removes it
+    assert(s.length == N-1);
+    assert(s != duplicate);
+    assert(forty2 !in s);
+    assert(s.remove(forty2) == false); // does nothing, it was already removed
+
+    foreach (MyInt* i; s) {
+        if (i != null) assert(list.canFind(i.value));
+    }
+    assert(usingCustomHash);
+}
+
+/// Builds a [Set] from a finite input range.
+static Set!(ElementType!Range) set(Range)(Range inputs)
+if (isInputRange!Range && !isInfinite!Range)
+{
+    alias T = ElementType!Range;
+    Set!T set;
+    foreach (x; inputs) set.add(x);
+    return set;
+}
+
+
+/++
+Generic pointer-like wrapper used to forward overloads to its pointed-to object.
+
+This behaviour can be useful when one wishes to use references as hash table keys: whereas the default hash for pointers is solely based on the address being pointed to, using this wrapper as a key will call the pointed-to-type's custom hash function instead (if it is defined).
+There is no overhead associated with using this wrapper.
++/
+struct HashablePointer(T) if (!isPointer!T) {
+    T* ptr;
+    alias ptr this; // dispatches most pointer-like behaviour to actual pointer
+    static assert((HashablePointer!T).sizeof == (T*).sizeof, "HashablePointer!T should have the sizeof a T*");
+
+ nothrow pure @safe:
+    inout this(return inout(T)* ptr) @nogc {
+        this.ptr = ptr;
+    }
+
+    size_t toHash() const {
+        return ptr == null ? 0 : typeid(T).getHash(ptr);
+    }
+
+    bool opEquals()(auto ref const(HashablePointer!T) rhs) const {
+        if (this.ptr == rhs.ptr) return true;
+        else return this.ptr != null && rhs.ptr != null && *this.ptr == *rhs.ptr;
+    }
+
+    bool opEquals(const(T)* rhs) const @nogc {
+        return this.ptr == rhs;
+    }
+}
+
+///
+@nogc nothrow pure unittest {
+    struct Foo {
+        bool baz = false;
+        string bar(const(Foo)* f) const @nogc nothrow { return "ok"; }
+    }
+
+    // a HashablePointer!T works just like a T* would
+    Foo x;
+    HashablePointer!Foo p = &x;
+    assert(p == &x);
+    assert(*p == x);
+    assert(p.baz == x.baz);
+    assert(p.bar(p) == x.bar(&x)); // implicit conversion
+    p = null;
+    assert(p == null);
+}
